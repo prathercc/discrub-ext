dd749ba473fe08ac14c089ccefcd7d8ef2946385
prathercc <prathercc@gmail.com>
2022-08-23 11:32:23 -0700

    Update screenshots, increase version to 1.1.1, and update changelog

--- src/components/InjectedDialog/InjectedDialog.jsx
+++ src/components/InjectedDialog/InjectedDialog.jsx
@@ -46,7 +46,7 @@ function InjectedDialog() {
       {menuIndex === 1 && <DirectMessages />}
       {menuIndex === 2 && <About />}
       <Box className={classes.box}>
-        <Typography>Version 1.1.0</Typography>
+        <Typography>Version 1.1.1</Typography>
       </Box>
       <CloseWindowButton />
     </Box>
