4bdeece95c6f7c0bbaee2e3ad6fe1962e8de1b20
prathercc <prathercc@gmail.com>
2022-11-15 07:21:50 -0800

    Start ExportGuild component

---
+++ src/components/ChannelMessages/ExportGuild.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "@mui/material";
+// import ChannelMessagesStyles from "./ChannelMessages.styles";
+
+const ExportGuild = () => {
+  //   const classes = ChannelMessagesStyles();
+
+  return (
+    <>
+      <Button variant="contained">Export</Button>
+    </>
+  );
+};
+
+export default ExportGuild;
