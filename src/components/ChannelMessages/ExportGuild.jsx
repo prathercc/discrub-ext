f465a4eefd6928d315054a659853c418ba98e74f
prathercc <prathercc@gmail.com>
2022-11-15 23:13:34 -0800

    Setup button and menu options for ExportGuild

--- src/components/ChannelMessages/ExportGuild.jsx
+++ src/components/ChannelMessages/ExportGuild.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import { Button } from "@mui/material";
-// import ChannelMessagesStyles from "./ChannelMessages.styles";
+import React, { useState } from "react";
+import { Button, Menu, MenuItem, Divider, Tooltip } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ChannelMessagesStyles from "./ChannelMessages.styles";
 
 const ExportGuild = () => {
-  //   const classes = ChannelMessagesStyles();
+  const classes = ChannelMessagesStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = !!anchorEl;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
-      <Button variant="contained">Export</Button>
+      <Tooltip title="Local options will inlude attachments alongside messages">
+        <Button
+          variant="contained"
+          disableElevation
+          onClick={handleClick}
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          Export
+        </Button>
+      </Tooltip>
+
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem dense onClick={handleClose}>
+          HTML (Local)
+        </MenuItem>
+        <MenuItem dense onClick={handleClose}>
+          JSON (Local)
+        </MenuItem>
+        <Divider />
+        <MenuItem dense onClick={handleClose}>
+          HTML
+        </MenuItem>
+        <MenuItem dense onClick={handleClose}>
+          JSON
+        </MenuItem>
+      </Menu>
     </>
   );
 };
