dfc9a0664abb06e20b0c681059f81ac226c9d3b7
prathercc <prathercc@gmail.com>
2023-03-12 17:54:38 -0700

    Add fetchDonationData service function

--- src/announcementService.js
+++ src/announcementService.js
@@ -1,4 +1,4 @@
-export const fetchAnnouncementData = (authorization) => {
+export const fetchAnnouncementData = () => {
   return fetch(
     `https://api.github.com/gists/912abef788d8f6dc25b3f4e0fa5e6371`,
     {
@@ -16,4 +16,21 @@ export const fetchAnnouncementData = (authorization) => {
       return announcement;
     })
     .catch((e) => console.error("Error fetching announcement", e));
+};
+export const fetchDonationData = () => {
+  return fetch(
+    `https://api.github.com/gists/2aca7928b4db1ab84eac3720ac8e8559`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then(async (resp) => {
+      const gistData = await resp.json();
+      const donation = JSON.parse(gistData.files["donation.json"]?.content);
+      return donation;
+    })
+    .catch((e) => console.error("Error fetching donation", e));
 };
