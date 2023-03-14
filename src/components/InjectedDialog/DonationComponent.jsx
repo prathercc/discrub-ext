e4162d6a1c4fe0c6ad10c311ae8b9a85d9ad3182
prathercc <prathercc@gmail.com>
2023-03-13 19:44:31 -0700

    Add arrows

--- src/components/InjectedDialog/DonationComponent.jsx
+++ src/components/InjectedDialog/DonationComponent.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
-import { Avatar, Stack, Typography } from "@mui/material";
+import { Avatar, Stack, Typography, IconButton } from "@mui/material";
 import DonationComponentStyles from "./DonationComponent.styles";
 import { fetchDonationData } from "../../announcementService";
 import { differenceInDays, parseISO } from "date-fns";
 import CoffeeIcon from "@mui/icons-material/Coffee";
 import Tooltip from "../DiscordComponents/DiscordTooltip/DiscordToolTip";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 function DonationComponent() {
   const classes = DonationComponentStyles();
   const [donations, setDonations] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
   useEffect(() => {
     const getDonationData = async () => {
       const data = await fetchDonationData();
@@ -26,7 +29,33 @@ function DonationComponent() {
       alignItems="center"
       spacing={2}
     >
-      {donations.map((donation) => (
+      <Box className={classes.arrowBox}>
+        <IconButton
+          disabled={startIndex === 0}
+          onClick={() => {
+            if (startIndex > 0) {
+              setStartIndex(startIndex - 3);
+            }
+          }}
+          color="secondary"
+        >
+          <ArrowBackIcon />
+        </IconButton>
+        <IconButton
+          disabled={
+            startIndex === donations.length - 1 || !donations[startIndex + 3]
+          }
+          onClick={() => {
+            if (startIndex < donations.length - 1) {
+              setStartIndex(startIndex + 3);
+            }
+          }}
+          color="secondary"
+        >
+          <ArrowForwardIcon />
+        </IconButton>
+      </Box>
+      {donations.slice(startIndex, startIndex + 3).map((donation) => (
         <Tooltip description={`"${donation.message}"`}>
           <Box className={classes.box}>
             <Stack
--- src/components/InjectedDialog/DonationComponent.styles.js
+++ src/components/InjectedDialog/DonationComponent.styles.js
@@ -7,7 +7,10 @@ const DonationComponentStyles = makeStyles(() => ({
     backgroundColor: "#2f3136",
     border: "1px solid #202225",
     borderRadius: "10px",
-    "& .MuiTypography-root": { color: "#d0d3f5 !important" },
+    "& .MuiTypography-root": {
+      color: "#d0d3f5 !important",
+      userSelect: "none !important",
+    },
     "& .MuiAvatar-root": {
       color: "#2f3136 !important",
       backgroundColor: "#5865f2 !important",
@@ -19,6 +22,16 @@ const DonationComponentStyles = makeStyles(() => ({
     bottom: "7px",
     right: "27px",
     width: "1200px",
+  },
+  arrowBox: {
+    position: "fixed",
+    bottom: "59px",
+    right: "190px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "865px",
+    justifyContent: "space-between",
   },
 }));
 
