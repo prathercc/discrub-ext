5db1f51e277bb0c13687bff2f4114fc7d1edcdd0
prathercc <prathercc@gmail.com>
2023-04-22 12:21:18 -0700

    Allow UserIds to be manually typed for Channel Messages username prefilter.

--- src/components/AdvancedFiltering/AdvancedFiltering.jsx
+++ src/components/AdvancedFiltering/AdvancedFiltering.jsx
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
-import MenuItem from "@mui/material/MenuItem";
-import { Stack, TextField, Button, Collapse } from "@mui/material";
-import Tooltip from "../DiscordComponents/DiscordTooltip/DiscordToolTip";
+import { Stack, Button, Collapse } from "@mui/material";
 import { ChannelContext } from "../../context/channel/ChannelContext";
 import { MessageContext } from "../../context/message/MessageContext";
 import { DmContext } from "../../context/dm/DmContext";
 import AdvancedFilteringStyles from "./AdvancedFiltering.styles";
 import BeforeAndAfterFields from "../Fields/BeforeAndAfterFields";
+import PrefilterUser from "./PrefilterUser";
 
 function AdvancedFiltering({
   closeAnnouncement = () => {},
@@ -15,20 +14,14 @@ function AdvancedFiltering({
   showOptionalFilters,
 }) {
   const { state: messageDataState } = useContext(MessageContext);
-  const { state: channelState, setPreFilterUserId } =
-    useContext(ChannelContext);
-  const { state: dmState, setPreFilterUserId: setDmPrefilterUserId } =
-    useContext(DmContext);
+  const { state: channelState } = useContext(ChannelContext);
+  const { state: dmState } = useContext(DmContext);
 
-  const classes = AdvancedFilteringStyles({ showOptionalFilters });
+  const classes = AdvancedFilteringStyles();
 
-  const { selectedChannel, preFilterUserIds, preFilterUserId } = channelState;
+  const { selectedChannel } = channelState;
   const { isLoading: messagesLoading } = messageDataState;
-  const {
-    selectedDm,
-    preFilterUserId: dmPreFilterUserId,
-    preFilterUserIds: dmPreFilterUserIds,
-  } = dmState;
+  const { selectedDm } = dmState;
 
   const handleFilterButtonClick = () => {
     closeAnnouncement();
@@ -57,42 +50,7 @@ function AdvancedFiltering({
           alignItems="center"
           spacing={1}
         >
-          <Tooltip
-            arrow
-            title="Filter By Username"
-            description="Due to API limitations, Channel Messages can only be pre-filtered by your username"
-            placement="top"
-          >
-            <TextField
-              className={classes.filterByUserName}
-              size="small"
-              fullWidth
-              variant="filled"
-              disabled={
-                (isDm ? selectedDm : selectedChannel).id === null ||
-                messagesLoading
-              }
-              value={isDm ? dmPreFilterUserId : preFilterUserId}
-              onChange={(e) =>
-                isDm
-                  ? setDmPrefilterUserId(e.target.value)
-                  : setPreFilterUserId(e.target.value)
-              }
-              select
-              label="Filter By Username"
-            >
-              <MenuItem dense value={null} key={-1}>
-                <strong>Reset Selection</strong>
-              </MenuItem>
-              {(isDm ? dmPreFilterUserIds : preFilterUserIds).map((user) => {
-                return (
-                  <MenuItem dense key={user.id} value={user.id}>
-                    {user.name}
-                  </MenuItem>
-                );
-              })}
-            </TextField>
-          </Tooltip>
+          <PrefilterUser isDm={isDm} />
           <BeforeAndAfterFields
             disabled={
               (isDm ? selectedDm : selectedChannel).id === null ||
