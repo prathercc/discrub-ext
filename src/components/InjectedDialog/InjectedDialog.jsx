f87414cd3a0c3249c39cf0700716953ad363339c
prathercc <prathercc@gmail.com>
2023-05-15 20:30:39 -0700

    Bump version

--- src/components/InjectedDialog/InjectedDialog.jsx
+++ src/components/InjectedDialog/InjectedDialog.jsx
@@ -116,7 +116,7 @@ function InjectedDialog() {
           justifyContent="center"
           spacing={1}
         >
-          <Typography variant="body2">1.10.3</Typography>
+          <Typography variant="body2">1.10.4</Typography>
         </Stack>
       </Box>
       <CloseWindowButton />
