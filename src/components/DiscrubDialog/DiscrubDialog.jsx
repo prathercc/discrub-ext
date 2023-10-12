97d55de191765aca49f56cb5fc62ef12645cb4c9
prathercc <prathercc@gmail.com>
2023-10-11 17:37:16 -0700

    Bump version, update screenshots

--- src/components/DiscrubDialog/DiscrubDialog.jsx
+++ src/components/DiscrubDialog/DiscrubDialog.jsx
@@ -118,7 +118,7 @@ function DiscrubDialog() {
           justifyContent="center"
           spacing={1}
         >
-          <Typography variant="body2">1.10.18</Typography>
+          <Typography variant="body2">1.11.0</Typography>
         </Stack>
       </Box>
       <CloseWindowButton />
