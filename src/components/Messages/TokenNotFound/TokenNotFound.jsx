746f8e616725207c60cac6326b48134e339a8856
prathercc <prathercc@gmail.com>
2023-10-11 17:36:17 -0700

    Provide alternate way to authorize into Discrub

--- src/components/Messages/TokenNotFound/TokenNotFound.jsx
+++ src/components/Messages/TokenNotFound/TokenNotFound.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import { Stack, Typography, Paper } from "@mui/material";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import {
+  Stack,
+  Typography,
+  Paper,
+  TextField,
+  Button,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import ChannelMessagesStyles from "../ChannelMessages/Styles/ChannelMessages.styles";
 import NoEncryptionGmailerrorredIcon from "@mui/icons-material/NoEncryptionGmailerrorred";
+import CloseIcon from "@mui/icons-material/Close";
+import { UserContext } from "../../../context/user/UserContext";
 
 function TokenNotFound() {
   const classes = ChannelMessagesStyles();
+  const inputRef = useRef(null);
+  const { getUserDataManually, state: userState } = useContext(UserContext);
+  const { isLoading } = userState;
+
+  const [token, setToken] = useState("");
+  const [authFailed, setAuthFailed] = useState(false);
+
+  const handleSubmitToken = async () => {
+    const { successful } = await getUserDataManually(token);
+    if (!successful) setAuthFailed(true);
+  };
+
+  const handleTokenUpdate = (e) => {
+    setToken(e.target.value);
+    setAuthFailed(false);
+  };
+
+  useEffect(() => {
+    if (!token) {
+      inputRef.current.value = null;
+    }
+  }, [token]);
 
   return (
     <Paper justifyContent="center" className={classes.paper}>
@@ -21,11 +53,53 @@ function TokenNotFound() {
           spacing={1}
         >
           <NoEncryptionGmailerrorredIcon />
-          <Typography variant="h5">Authentication Failed</Typography>
+          <Typography variant="h5">Authorization Failed</Typography>
         </Stack>
         <Typography variant="caption">
-          You must be signed into Discord to use this extension
+          Please sign into Discord or manually enter an Authorization Token
+          below:
         </Typography>
+        <Stack
+          sx={{ width: "100%" }}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <TextField
+            inputRef={inputRef}
+            error={authFailed}
+            helperText={
+              authFailed
+                ? "Session could not be found with the provided token"
+                : ""
+            }
+            size="small"
+            fullWidth
+            variant="filled"
+            label="Authorization Token"
+            onChange={handleTokenUpdate}
+            InputProps={{
+              disableUnderline: true,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => handleTokenUpdate({ target: { value: "" } })}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            disabled={isLoading || !token || token.length === 0}
+            onClick={handleSubmitToken}
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </Stack>
       </Stack>
     </Paper>
   );
