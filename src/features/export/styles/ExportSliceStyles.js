4bc5a3af12de6d013ab4336eba13789068e2b69c
prathercc <prathercc@gmail.com>
2023-12-04 21:59:45 -0800

    Update emoji styles, remove MessageMock.styles.js

---
+++ src/features/export/styles/ExportSliceStyles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@mui/styles";
+
+const ExportSliceStyles = makeStyles(() => ({
+  emoji: {
+    display: "inline-flex",
+    verticalAlign: "middle",
+  },
+  emojiImgDefault: {
+    width: "25px",
+    height: "25px",
+  },
+  emojiImgSmall: {
+    width: "16px",
+    height: "16px",
+  },
+}));
+
+export default ExportSliceStyles;
