6602377559f70cb3bec91bc25ac2044b17b6babf
VendorAttestation <132447890+VendorAttestation@users.noreply.github.com>
2024-10-19 00:32:55 -0500

    Update button-injection.tsx

--- src/button-injection.tsx
+++ src/button-injection.tsx
@@ -1,13 +1,18 @@
-import ReactDOM from "react-dom/client";
-import DiscrubButton from "./containers/discrub-button/discrub-button.tsx";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme.ts";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DiscrubButton from './containers/discrub-button/discrub-button';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme';
 
-const injectionButton = document.getElementById("button_injection");
-if (injectionButton) {
-  ReactDOM.createRoot(injectionButton).render(
-    <ThemeProvider theme={theme}>
-      <DiscrubButton />
-    </ThemeProvider>
-  );
+let injectionButton = document.getElementById('button_injection');
+if (!injectionButton) {
+  injectionButton = document.createElement('div');
+  injectionButton.id = 'button_injection';
+  document.body.appendChild(injectionButton);
 }
+
+ReactDOM.createRoot(injectionButton).render(
+  <ThemeProvider theme={theme}>
+    <DiscrubButton />
+  </ThemeProvider>
+);
