b0658ab6cccc68a63cbf9741abaa8a9490a7e2e7
prathercc <prathercc@gmail.com>
2024-11-29 22:25:42 -0800

    Support categories in MultiValueSelect component

--- src/common-components/multi-value-select/multi-value-select.tsx
+++ src/common-components/multi-value-select/multi-value-select.tsx
@@ -7,7 +7,9 @@ import {
   Checkbox,
   ListItemText,
   SelectChangeEvent,
+  ListSubheader,
 } from "@mui/material";
+import { ReactNode } from "react";
 
 type MultiValueSelectProps = {
   disabled?: boolean;
@@ -16,6 +18,8 @@ type MultiValueSelectProps = {
   onChange: (values: string[]) => void;
   values: string[];
   displayNameMap?: Record<string, string>;
+  categories?: string[];
+  categoryMap?: Record<string, string>;
 };
 
 const MultiValueSelect = ({
@@ -25,6 +29,8 @@ const MultiValueSelect = ({
   onChange,
   values,
   displayNameMap,
+  categories,
+  categoryMap,
 }: MultiValueSelectProps) => {
   const handleChange = (e: SelectChangeEvent<string[]>) => {
     const { value } = e.target;
@@ -35,8 +41,29 @@ const MultiValueSelect = ({
     }
   };
 
+  const getMenuItem = (name: string) => (
+    <MenuItem dense key={name} value={name}>
+      <Checkbox size="small" checked={value?.indexOf(name) > -1} />
+      <ListItemText
+        primary={
+          displayNameMap && displayNameMap[name] ? displayNameMap[name] : name
+        }
+      />
+    </MenuItem>
+  );
+
+  const elements: ReactNode[] = !!(categoryMap && categories?.length)
+    ? categories.reduce((acc: ReactNode[], curr) => {
+        return [
+          ...acc,
+          <ListSubheader>{curr}</ListSubheader>,
+          ...values.filter((v) => categoryMap[v] === curr).map(getMenuItem),
+        ];
+      }, [])
+    : values.map(getMenuItem);
+
   return (
-    <FormControl sx={{ width: "100%" }}>
+    <FormControl sx={{ width: "100%" }} size="small">
       <InputLabel
         variant="filled"
         size="small"
@@ -60,18 +87,7 @@ const MultiValueSelect = ({
           ).join(", ")
         }
       >
-        {values.map((name) => (
-          <MenuItem dense key={name} value={name}>
-            <Checkbox size="small" checked={value?.indexOf(name) > -1} />
-            <ListItemText
-              primary={
-                displayNameMap && displayNameMap[name]
-                  ? displayNameMap[name]
-                  : name
-              }
-            />
-          </MenuItem>
-        ))}
+        {elements.map((e) => e)}
       </Select>
     </FormControl>
   );
