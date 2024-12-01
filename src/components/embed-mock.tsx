a0f21e5b3f7702a86f3f64f56a7d4a8e9a188b66
prathercc <prathercc@gmail.com>
2024-11-30 22:55:13 -0800

    Refactor AttachmentMock and EmbedMock components

--- src/components/embed-mock.tsx
+++ src/components/embed-mock.tsx
@@ -1,18 +1,19 @@
-import AttachmentMock from "./attachment-mock";
-import Attachment from "../classes/attachment";
 import Embed from "../classes/embed";
 import { useExportSlice } from "../features/export/use-export-slice";
 import { EmbedType } from "../enum/embed-type";
 import { useAppSlice } from "../features/app/use-app-slice";
 import { stringToTypedArray } from "../utils";
 import { MediaType } from "../enum/media-type";
+import VideoMock from "./video-mock.tsx";
+import RemoteVideoMock from "./remote-video.tsx";
+import ImageMock from "./image-mock.tsx";
 
 type EmbedMockProps = {
   embed: Embed;
   index: number;
 };
 
-const EmbedMock = ({ embed, index }: EmbedMockProps) => {
+const EmbedMock = ({ embed }: EmbedMockProps) => {
   const { type, video, description, thumbnail } = embed;
 
   const { state: appState } = useAppSlice();
@@ -27,12 +28,18 @@ const EmbedMock = ({ embed, index }: EmbedMockProps) => {
   const isPreviewingVideos = previewMedia.some(
     (mt) => mt === MediaType.EMBEDDED_VIDEOS,
   );
+  const resMode = settings.exportImageResMode;
   const mediaMap = exportState.mediaMap();
 
   const videoUrl =
     mediaMap[String(embed.video?.proxy_url)] ||
     embed.video?.proxy_url ||
+    embed.video?.url ||
     undefined;
+  const imageUrl =
+    mediaMap[String(thumbnail?.proxy_url)] ||
+    thumbnail?.proxy_url ||
+    thumbnail?.url;
 
   const embedWidth = Number(video?.width);
   const embedHeight = Number(video?.height);
@@ -40,54 +47,29 @@ const EmbedMock = ({ embed, index }: EmbedMockProps) => {
   return (
     <>
       {isPreviewingVideos && type === EmbedType.GIFV && video && (
-        <video
-          style={{
-            borderRadius: "10px",
-            border: "1px solid transparent",
-            boxShadow: "4px 5px 13px 0px rgba(0,0,0,0.75)",
-          }}
-          width={embedWidth > 400 ? 400 : embedWidth}
-          height={embedHeight > 225 ? 225 : embedHeight}
-          src={videoUrl}
+        <VideoMock
+          url={videoUrl}
+          width={embedWidth}
+          height={embedHeight}
           loop
-          controls
-          playsInline
-          autoPlay={false}
-          poster="../discrub_media/discrub.png" // Required to prevent 'react-to-print' from hanging indefinitely in some cases.
         />
       )}
-      {isPreviewingVideos && type === EmbedType.VIDEO && video && (
-        <iframe
-          style={{
-            borderRadius: "10px",
-            border: "1px solid transparent",
-            boxShadow: "4px 5px 13px 0px rgba(0,0,0,0.75)",
-          }}
-          width="400"
-          height="225"
-          src={`${video.url}?origin=${window.location.href}`}
-          title={description}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      )}
+      {isPreviewingVideos &&
+        type === EmbedType.VIDEO &&
+        video &&
+        (video.proxy_url ? (
+          <VideoMock url={videoUrl} width={embedWidth} height={embedHeight} />
+        ) : (
+          <RemoteVideoMock url={videoUrl} description={description} />
+        ))}
       {isPreviewingImages &&
         [EmbedType.IMAGE, EmbedType.ARTICLE].some((t) => t === type) &&
         thumbnail && (
-          <AttachmentMock
-            attachment={
-              new Attachment({
-                content_type: "image/jpeg",
-                filename: String(embed.title),
-                height: thumbnail.height,
-                id: `EMBEDDED-MEDIA-${index}`,
-                size: 0,
-                url: thumbnail.url,
-                proxy_url: thumbnail.proxy_url || thumbnail.url,
-                width: thumbnail.width,
-              })
-            }
+          <ImageMock
+            url={imageUrl}
+            width={Number(thumbnail.width)}
+            height={Number(thumbnail.height)}
+            resMode={resMode}
           />
         )}
     </>
