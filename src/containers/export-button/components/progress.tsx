0733e4e2e81611b6cc90ba35530b7d60819a298b
prathercc <prathercc@gmail.com>
2025-06-25 23:25:01 -0700

    #XXX - Update progress.tsx such that changes to status are tracked and easily viewable during exports

--- src/containers/export-button/components/progress.tsx
+++ src/containers/export-button/components/progress.tsx
@@ -1,14 +1,49 @@
-import { Stack, Typography, LinearProgress } from "@mui/material";
+import {
+  Stack,
+  Typography,
+  LinearProgress,
+  Box,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import { useExportSlice } from "../../../features/export/use-export-slice";
 import { useAppSlice } from "../../../features/app/use-app-slice";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import { useEffect, useState } from "react";
 
 const Progress = () => {
   const { state: appState } = useAppSlice();
   const task = appState.task();
+  const [statusMessages, setStatusMessages] = useState<string[]>([]);
   const { statusText } = task || {};
 
   const { state: exportState } = useExportSlice();
   const name = exportState.name();
+
+  useEffect(() => {
+    if (statusText) {
+      setStatusMessages((prevState) =>
+        [statusText, ...prevState].slice(0, 200),
+      );
+    }
+  }, [statusText]);
+
+  const getUserListItem = (props: ListChildComponentProps) => {
+    const { index, style } = props;
+    return (
+      <ListItem
+        dense
+        style={style}
+        key={statusMessages[index]}
+        sx={{
+          height: 30,
+          whiteSpace: "nowrap",
+        }}
+      >
+        <ListItemText>{statusMessages[index]}</ListItemText>
+      </ListItem>
+    );
+  };
 
   return (
     <Stack
@@ -20,7 +55,23 @@ const Progress = () => {
     >
       <Typography>{name}</Typography>
       <LinearProgress sx={{ width: "100%", m: 1 }} />
-      <Typography variant="caption">{statusText}</Typography>
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 500,
+          height: 300,
+          backgroundColor: "background.paper",
+        }}
+      >
+        <FixedSizeList
+          height={300}
+          width={500}
+          itemSize={30}
+          itemCount={statusMessages.length}
+        >
+          {getUserListItem}
+        </FixedSizeList>
+      </Box>
     </Stack>
   );
 };
